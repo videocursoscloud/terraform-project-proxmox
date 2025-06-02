7a33b6d2fc3d9a695aaaced9d71d2044c4cfbe23
mijack <mijack@qnap.homelab.localdomain>
2025-06-02 13:36:30 +0000

    changes

--- k8s.tf
+++ k8s.tf
@@ -1,8 +1,6 @@
 locals {
   kubernetes = {
     k8s-cp-1   = { ip = "10.0.55.131/24", target_node = "microserver55", memory = 4096, balloon = 4096 }
-    k8s-cp-2   = { ip = "10.0.55.132/24", target_node = "microserver56", memory = 4096, balloon = 4096 }
-    k8s-cp-3   = { ip = "10.0.55.133/24", target_node = "microserver57", memory = 4096, balloon = 4096 }
     k8s-node-1 = { ip = "10.0.55.141/24", target_node = "microserver55", memory = 4096, balloon = 4096 }
     k8s-node-2 = { ip = "10.0.55.142/24", target_node = "microserver56", memory = 4096, balloon = 4096 }
     k8s-node-3 = { ip = "10.0.55.143/24", target_node = "microserver57", memory = 4096, balloon = 4096 }
